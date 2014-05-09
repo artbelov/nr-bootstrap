460bfdccb48f872fe024dfa999ca09874f6efba8
Artem Belov <artbelov@live.ru>
2014-05-09 15:29:27 +0400

    D.R.Y. "main.js"

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,7 @@
-/*!
- * Non-responsive Bootstrap v3.1.1
+/**
+ * Non-responsive Bootstrap "gh-pages" JS
  * Homepage: http://www.artbelov.com/non-responsive-bootstrap/
- * Based on Bootstrap (http://getbootstrap.com)
  * Copyright 2014 Artem Belov
- * Licensed under MIT (http://opensource.org/licenses/MIT)
  */
 
 /* global jQuery:false */
@@ -15,71 +13,48 @@
   // Change Width of Container
   $(function () {
 
-    // Set Uo Values
+    // SetUp Default Values
     var step = 10,
-      minval = 800,
-      maxval = 1140;
+        minval = 800,
+        maxval = 1140;
 
     // Change Width Function
-    function changeWidth(width) {
-      // Set value
-      $('.qty').val(width);
-      // Inject CSS
-      $('.cover-container, .masthead, .mastfoot').css('width', width);
-    }
-
-    // This button will increment the value
-    $('.qtyplus, .qtyminus').click(function (e) {
-      // Get current value
-      var curVal = parseInt($('.qty').val());
-      // Stop acting like a button
-      e.preventDefault();
+    var changeWidth = function (width, qtyplus, qtyminus) {
+      // Set Values
+      var setVal = function (px) {
+        $('.qty').val(px);
+        $('.cover-container, .masthead, .mastfoot').css('width', px);
+      };
       // If is correct number
-      if (!isNaN(curVal) && curVal >= minval && curVal <= maxval) {
-        if ($(this).hasClass('qtyplus') && curVal < maxval) {
-          // Increment
-          changeWidth(curVal + step);
-        } else if ($(this).hasClass('qtyminus') && curVal > minval) {
-          // Decrement
-          changeWidth(curVal - step);
+      if (!isNaN(width)) {
+        if (qtyplus && width < maxval) {
+          setVal(width + step);
+        } else if (qtyminus && width > minval) {
+          setVal(width - step);
+        } else if (width > maxval) {
+          setVal(maxval);
+        } else if (width < minval) {
+          setVal(minval);
+        } else {
+          setVal(width);
         }
-      } else if (!isNaN(curVal) && curVal > maxval) {
-        // Otherwise put a maxval there
-        changeWidth(maxval);
-      } else if (!isNaN(curVal) && curVal < minval) {
-        // Otherwise put a minval there
-        changeWidth(minval);
       }
+    };
+
+    // Plus-Minus Buttons and Input
+    $('.qty, .qtyplus, .qtyminus').on('click blur', function () {
+      changeWidth(parseInt($('.qty').val()), $(this).hasClass('qtyplus'), $(this).hasClass('qtyminus'));
     });
 
-    // Disallow Characters in input
+    // Disallow Characters in Input
     $('.qty').keydown(function (e) {
-      // Allow: backspace, delete, tab, escape, enter and .
-      if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
-        // Allow: Ctrl+A
-        (e.keyCode == 65 && e.ctrlKey === true) ||
-        // Allow: home, end, left, right
-        (e.keyCode >= 35 && e.keyCode <= 39)) {
-        // Let it happen, don't do anything
+      // Allow: (backspace, delete, tab, escape, enter, . ) and (Ctrl+A) and (home, end, left, right)
+      if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 || (e.keyCode == 65 && e.ctrlKey === true) || (e.keyCode >= 35 && e.keyCode <= 39)) {
         return;
       }
       // Ensure that it is a number and stop the keypress
       if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
         e.preventDefault();
-      }
-    });
-
-    // On Blur Event
-    $('.qty').blur(function () {
-      // Get its current value
-      var curVal = parseInt($(this).val());
-      // Check current value
-      if (!isNaN(curVal) && curVal > minval && curVal < maxval) {
-        changeWidth(curVal);
-      } else if (!isNaN(curVal) && curVal < minval) {
-        changeWidth(minval);
-      } else if (!isNaN(curVal) && curVal > maxval) {
-        changeWidth(maxval);
       }
     });
 
