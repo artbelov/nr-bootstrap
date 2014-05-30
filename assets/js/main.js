49de61c435f196762c40eab7ca6aea5ff35ad44b
Artem Belov <artbelov@live.ru>
2014-05-30 23:42:34 +0400

    Pure JS Realization

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,64 +4,71 @@
  * Copyright 2014 Artem Belov
  */
 
-/* global jQuery:false */
+// Set Default Values
+var step = 10,
+  minVal = 800,
+  maxVal = 1140,
+  scrollBar = 20,
+  viewPort = screen.width,
+  iqty = document.getElementById('qty'),
+  iqtyplus = document.getElementById('qtyplus'),
+  iqtyminus = document.getElementById('qtyminus'),
+  icoverContainer = document.getElementById('cover-container'),
+  imastFoot = document.getElementById('mastfoot');
 
-(function ($, window, document, undefined) {
 
-  'use strict';
+// Set Maximum Container Width
+if (viewPort < maxVal) {
+  maxVal = Math.floor(viewPort / 100) * 100 - scrollBar;
+}
 
-  // Change Width of Container
-  $(function () {
-
-    // Set Default Values
-    var step = 10, minVal = 800, maxVal = 1140, viewPort = $(window).width(), scrollBar = 20;
-    
-    // Set Maximum Container Width
-    if (viewPort < maxVal) {
-      maxVal = Math.floor(viewPort / 100) * 100 - scrollBar;
-    }
-
-    // Change Width Function
-    var changeWidth = (function (width, qtyplus, qtyminus) {
-      // Set Width and Input Value
-      var setVal = (function (width) {
-        $('.qty').val(width);
-        $('.cover-container, .masthead, .mastfoot').css('width', width);
-      });
-      // If is correct number
-      if (!isNaN(width)) {
-        if (qtyplus && width < maxVal) {
-          setVal(width + step);
-        } else if (qtyminus && width > minVal) {
-          setVal(width - step);
-        } else if (width > maxVal) {
-          setVal(maxVal);
-        } else if (width < minVal) {
-          setVal(minVal);
-        } else {
-          setVal(width);
-        }
-      }
-    });
-
-    // Plus-Minus Buttons
-    $('.qtyplus, .qtyminus').on('click', function () {
-      changeWidth(parseInt($('.qty').val()), $(this).hasClass('qtyplus'), $(this).hasClass('qtyminus'));
-    });
-
-    // Width Input
-    $('.qty').on('blur', function () {
-      changeWidth(parseInt($('.qty').val()));
-    });
-
-    // Disallow Characters in Width Input
-    $('.qty').keydown(function (e) {
-      // Allow keys: backspace, home, end, left, right, delete and Ctrl+A
-      if ($.inArray(e.keyCode, [8, 35, 36, 37, 39, 46]) !== -1 || (e.ctrlKey === true && e.keyCode == 65)) { return; }
-      // Ensure that it is a number and stop the keypress
-      if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) { e.preventDefault(); }
-    });
-
+// Change Width Function
+var changeWidth = (function (width, btn, icon) {
+  // Set Width and Input Value
+  var setVal = (function (width) {
+    iqty.value = width;
+    icoverContainer.setAttribute('style', 'width:' + width + 'px');
+    imastFoot.setAttribute('style', 'width:' + width + 'px');
   });
+  // If is correct number
+  if (!isNaN(width)) {
+    if (btn == 'qtyplus' || icon == 'qtyplus' && width < maxVal) {
+      setVal(width + step);
+    } else if (btn == 'qtyminus' || icon == 'qtyminus' && width > minVal) {
+      setVal(width - step);
+    } else if (width > maxVal) {
+      setVal(maxVal);
+    } else if (width < minVal) {
+      setVal(minVal);
+    } else {
+      setVal(width);
+    }
+  }
+});
 
-})(jQuery, window, document);
+// Plus-Minus Buttons
+iqtyminus.onclick = function (e) {
+  changeWidth(parseInt(iqty.value), e.target.id, e.target.parentElement.id);
+};
+
+iqtyplus.onclick = function (e) {
+  changeWidth(parseInt(iqty.value), e.target.id, e.target.parentElement.id);
+};
+
+// Width Input
+iqty.onblur = function () {
+  changeWidth(iqty.value);
+};
+
+// Disallow Characters in Width Input
+iqty.onkeydown = function (e) {
+  // Allow keys: backspace, end, home, left, right, delete and Ctrl+A
+  var allowKeys = [8, 35, 36, 37, 39, 46];
+  if (allowKeys.indexOf(e.keyCode) !== -1 || (e.ctrlKey === true && e.keyCode == 65)) {
+    return;
+  }
+  // Ensure that it is a number and stop the keypress
+  if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+    e.preventDefault();
+  }
+};
